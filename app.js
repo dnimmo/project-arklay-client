a8044de2a4e4403b016885324897743062ed31e4
dnimmo <dnimmo@gmail.com>
2016-08-20 15:43:41 +0000

    basic react start

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 require('./style/main.scss')
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const testApp = params => (<div>{params}</div>)
+const test2 = () => <span>Hello</span>
+                           
+ReactDom.render(testApp(test2()), document.getElementById('app'))
