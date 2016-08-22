a20f0b11e413c3095457079d841c37becec6146c
David Nimmo <dnimmo@gmail.com>
2016-08-22 18:29:12 +0100

    only emit update event if data has changed

--- data-management/store.js
+++ data-management/store.js
@@ -1,4 +1,4 @@
-// All of the room/inventory data lives in here - all of the updates happen here, and any requests for the data come here
+// All of the room/inventory data lives in here - all of the updates happen here, and any requests for the data come through here
 const dataUpdated = new Event('data-updated')
 document.addEventListener('data-updated', (e) => { console.log('update', getData('inventory')) }, false)
 const emitUpdateEvent = () => document.dispatchEvent(dataUpdated)
@@ -9,9 +9,13 @@ const dataStore = {
 }
 
 const getData = attribute => dataStore[attribute]
+
 const updateData = (type, data) => {
+  // Update data, and emit event if updated data is different from pre-update data
+  const preUpdate = getData(type)
   dataStore[type] = data
-  emitUpdateEvent()
+  const postUpdate = getData(type)
+  if (preUpdate !== postUpdate) emitUpdateEvent()
 }
 
 module.exports = {
