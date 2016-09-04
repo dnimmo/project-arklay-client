75b0f9819402f58d4c06ea21ed6e76659ae2a0e3
David Nimmo <dnimmo@gmail.com>
2016-09-04 04:18:43 +0100

    removed unnecessary comment

--- dom-management/inventory-manager.js
+++ dom-management/inventory-manager.js
@@ -5,8 +5,6 @@ import inventoryPanel from  './components/inventory/inventory-panel'
 
 const updateInventoryUI = ( {inventoryClasses, itemListClasses, itemDetailsClasses, item = {}} ) => {
   const inventory = getData('inventory')
-  //
-  // if (inventory.items.length === 0) inventoryClasses = ['hidden']
 
   const inventoryObject = component({
     type: 'div',
