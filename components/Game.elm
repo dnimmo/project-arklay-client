9be880ee86715b9e3999bc1d2fb0b17e2667cf5a
David Nimmo <dnimmo@gmail.com>
2017-03-10 14:16:02 +0000

    refactor for room updates

--- components/Game.elm
+++ components/Game.elm
@@ -4,51 +4,76 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import List
-import Directions
+import Rooms
 
 
 type alias Model =
-    { room : String
-    , surroundings : String
-    , availableDirections : List Direction
+    { room : Room
     }
 
 
-type alias Direction =
-    Directions.Direction
+type alias Room =
+    Rooms.Model
 
 
 type Msg
-    = ChangeRoom
+    = ChangeRoom String
 
 
 initModel : Model
 initModel =
-    { room = "You are in the foyer of what appears to be a mansion."
-    , surroundings = "A vast, dimly-lit foyer, with a grand staircase and a couple of doors."
+    { room = Rooms.initModel
+    }
+
+
+getRoom : String -> Room
+getRoom roomName =
+    { name = "Next"
+    , intro = "updated"
+    , surroundings = "updated"
     , availableDirections =
-        [ { text = "North"
+        [ { text = "South"
           , destination = "Some Room"
           }
         ]
     }
 
 
+renderRoomInfo : Model -> Html Msg
+renderRoomInfo model =
+    div []
+        [ p []
+            [ text model.room.intro ]
+        , p []
+            [ text model.room.surroundings ]
+        ]
+
+
+renderDirectionOptions : Rooms.Direction -> Html Msg
+renderDirectionOptions direction =
+    li [ onClick (ChangeRoom direction.destination) ] [ text direction.text ]
+
+
+renderDirections : List Rooms.Direction -> Html Msg
+renderDirections availableDirections =
+    ul [ class "DirectionOptions" ]
+        (availableDirections
+            |> List.map renderDirectionOptions
+        )
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        ChangeRoom ->
+        ChangeRoom roomToChangeTo ->
             { model
-                | room = "Room changed"
+                | room = (getRoom roomToChangeTo)
             }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ p [ onClick ChangeRoom ]
-            [ text model.room ]
-        , p []
-            [ text model.surroundings ]
-        , Directions.renderDirections model.availableDirections
+        [ renderRoomInfo model
+        , renderDirections model.room.availableDirections
         ]
